0f2fda78c27ea6817fe583ef3808fd43a77bd44f
junhyeongleeee <djskal3745@gmail.com>
2023-01-05 19:40:35 +0900

    feat: MainBindingAdapter 추가

--- app/src/main/java/com/example/afreecatv/presentation/main/MainBindingAdapter.kt
+++ app/src/main/java/com/example/afreecatv/presentation/main/MainBindingAdapter.kt
@@ -2,7 +2,6 @@ package com.example.afreecatv.presentation.main
 
 import androidx.databinding.BindingAdapter
 import androidx.viewpager2.widget.ViewPager2
-import com.example.afreecatv.presentation.main.adapter.MainPagerAdapter
 import com.example.afreecatv.presentation.main.model.Category
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -12,9 +11,4 @@ fun TabLayout.initTab(viewPager2: ViewPager2) {
     TabLayoutMediator(this, viewPager2) { tab, position ->
         tab.text = Category.values()[position].category
     }.attach()
-}
-
-@BindingAdapter("MainPageAdapter")
-fun ViewPager2.initViewPager(mainPagerAdapter: MainPagerAdapter) {
-    adapter = mainPagerAdapter
 }
--- app/src/main/java/com/example/afreecatv/presentation/main/MainFragment.kt
+++ app/src/main/java/com/example/afreecatv/presentation/main/MainFragment.kt
@@ -4,15 +4,21 @@ import com.example.afreecatv.R
 import com.example.afreecatv.databinding.FragmentMainBinding
 import com.example.afreecatv.presentation.BaseFragment
 import com.example.afreecatv.presentation.main.adapter.MainPagerAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
 
     override fun initView() {
+        initViewPager()
         initBinding()
     }
 
-    private fun initBinding() {
+    private fun initViewPager() {
         binding.vpMain.adapter = MainPagerAdapter(requireActivity())
+    }
+
+    private fun initBinding() {
         binding.viewPager = binding.vpMain
     }
 }
