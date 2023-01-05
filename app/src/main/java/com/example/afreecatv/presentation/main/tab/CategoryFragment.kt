378681afc4fa6ad72af9f52b9ed9c1598a95bd0c
junhyeongleeee <djskal3745@gmail.com>
2023-01-05 19:41:07 +0900

    feat: Hilt 적용, CategoryViewModel 추가

--- app/src/main/java/com/example/afreecatv/presentation/main/tab/CategoryFragment.kt
+++ app/src/main/java/com/example/afreecatv/presentation/main/tab/CategoryFragment.kt
@@ -1,33 +1,28 @@
 package com.example.afreecatv.presentation.main.tab
 
 import android.os.Bundle
+import androidx.fragment.app.viewModels
 import com.example.afreecatv.R
 import com.example.afreecatv.databinding.FragmentCategoryBinding
 import com.example.afreecatv.presentation.BaseFragment
 import com.example.afreecatv.presentation.main.model.Category
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CategoryFragment : BaseFragment<FragmentCategoryBinding>(R.layout.fragment_category) {
 
-    private var param: Category? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param = it.getParcelable(ARG_KEY)
-        }
-    }
-
+    private val viewModel : CategoryViewModel by viewModels()
     override fun initView() {
-        binding.tvCategory.text = param?.category ?: ""
+        binding.viewModel = viewModel
     }
 
     companion object {
 
-        private const val ARG_KEY = "category"
+        const val KEY_CATEGORY = "category"
 
         fun newInstance(category: Category) = CategoryFragment().apply {
             arguments = Bundle().apply {
-                putParcelable(ARG_KEY, category)
+                putParcelable(KEY_CATEGORY, category)
             }
         }
     }
