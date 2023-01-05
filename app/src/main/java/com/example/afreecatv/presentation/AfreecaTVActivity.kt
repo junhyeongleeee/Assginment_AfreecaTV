36912bb25ed2c5a55d1693a490319fd143a864d4
junhyeongleeee <djskal3745@gmail.com>
2023-01-05 19:37:10 +0900

    chore: rename

---
+++ app/src/main/java/com/example/afreecatv/presentation/AfreecaTVActivity.kt
@@ -0,0 +1,10 @@
+package com.example.afreecatv.presentation
+
+import com.example.afreecatv.R
+import com.example.afreecatv.databinding.ActivityAfreecaBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AfreecaTVActivity : BaseActivity<ActivityAfreecaBinding>(R.layout.activity_afreeca) {
+    override fun initView() {}
+}
--- app/src/main/java/com/example/afreecatv/presentation/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.afreecatv.presentation
-
-import com.example.afreecatv.R
-import com.example.afreecatv.databinding.ActivityMainBinding
-
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-    override fun initView() {}
-}
