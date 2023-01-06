4dca75aba36c3f383aff1293e72986af3345ab45
junhyeongleeee <djskal3745@gmail.com>
2023-01-07 02:58:18 +0900

    feat: broad item UI 개선

--- app/src/main/java/com/example/afreecatv/util/LoadImage.kt
+++ app/src/main/java/com/example/afreecatv/util/LoadImage.kt
@@ -7,16 +7,20 @@ import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
+import com.example.afreecatv.R
 
 private val factory = DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()
 
-@BindingAdapter("setUrl")
-fun ImageView.loadImage(imageUrl: String?) {
+@BindingAdapter(value = ["setUrl", "setRadius"], requireAll = false)
+fun ImageView.loadImage(imageUrl: String?, cornerRadius: Int?) {
     imageUrl ?: return
 
     Glide.with(this)
         .load(imageUrl)
         .transition(DrawableTransitionOptions.withCrossFade(factory))
-        .transform(CenterCrop(), RoundedCorners(10))
+        .transform(
+            CenterCrop(),
+            RoundedCorners(cornerRadius ?: resources.getInteger(R.integer.radius_100))
+        )
         .into(this)
 }
